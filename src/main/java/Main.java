8c7f1feb90c1be22ce90a7de45bfa226482cdd45
Constantine Kim <electro.passion92@gmail.com>
2023-08-24 23:46:32 +0900

    Реализация входных парамметров для счетчика

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int personCount = 0;
+        System.out.println("На сколько человек необходимо разделить счёт?");
+        while (personCount <= 1) {
+            if (scanner.hasNextInt()) {
+                personCount = scanner.nextInt();
+                if (personCount <= 1) {
+                    System.out.println("Нет смысла делить счет");
+                }
+            }else {
+                System.out.println("Неверный формат данных");
+                scanner.next();
+            }
+        }
+        System.out.println(personCount);
     }
 }
