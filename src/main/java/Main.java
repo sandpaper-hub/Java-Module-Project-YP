d78a5ba78cd4c4d1d2b3f9fa9dfda03a3adcb7d5
Constantine Kim <electro.passion92@gmail.com>
2023-08-26 00:00:29 +0900

    Добавлены тесты

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         PeopleCounter peopleCounter = new PeopleCounter();
+        Calculator calculator = new Calculator();
         peopleCounter.countOfPeople(scanner);
+        calculator.addNewProduct(scanner);
     }
 }
