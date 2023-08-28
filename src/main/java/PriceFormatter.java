af8a6d7db9bf3591b26603bf9dba53c5ae198bb4
Constantine Kim <electro.passion92@gmail.com>
2023-08-28 16:04:02 +0900

    Добавлен класс, отвечающий за форматирование валюты

---
+++ src/main/java/PriceFormatter.java
@@ -0,0 +1,15 @@
+public class PriceFormatter {
+    String roundPrice(double price) {
+        return String.format("%.2f", price);
+    }
+
+    String rubInflection(double price) {
+        if (price % 10 == 1 && price % 100 != 11) {
+            return "рубль";
+        } else if (price % 10 >= 2 && price % 10 <= 4 || (price % 100 < 10 && price % 100 >= 20)) {
+            return "рубля";
+        } else {
+            return "рублей";
+        }
+    }
+}
