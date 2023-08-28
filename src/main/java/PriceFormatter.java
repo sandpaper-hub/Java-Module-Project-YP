0747af4414eff18c514866a53c8c6464d4fff702
Constantine Kim <electro.passion92@gmail.com>
2023-08-28 17:17:51 +0900

    -Синтаксические изменения

--- src/main/java/PriceFormatter.java
+++ src/main/java/PriceFormatter.java
@@ -5,11 +5,11 @@ public class PriceFormatter {
 
     String rubInflection(double price) {
         if (price % 10 == 1 && price % 100 != 11) {
-            return "рубль";
-        } else if (price % 10 >= 2 && price % 10 <= 4 || (price % 100 < 10 && price % 100 >= 20)) {
-            return "рубля";
+            return " рубль";
+        } else if (price % 10 >= 2 && price % 10 <= 4 && (price % 100 < 10 || price % 100 >= 20)) {
+            return " рубля";
         } else {
-            return "рублей";
+            return " рублей";
         }
     }
 }
